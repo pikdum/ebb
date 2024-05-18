94f93890acfda734e269dc3632290e1454679905
pikdum <pikdum@kuudere.moe>
2024-05-18 04:10:26 -0500

    feat: retries, error states, loading, etc.

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,22 @@ const booruSearch = async (
 	return data;
 };
 
+// https://tw-elements.com/docs/standard/components/spinners/
+const LoadingIndicator = ({ ...args }) => {
+	return (
+		<div {...args}>
+			<div
+				className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+				role="status"
+			>
+				<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+					Loading...
+				</span>
+			</div>
+		</div>
+	);
+};
+
 export const App = () => {
 	const headerRef = useRef(null);
 	const topRef = useRef(null);
@@ -20,22 +36,38 @@ export const App = () => {
 	const [page, setPage] = useState(0);
 	const [tempQuery, setTempQuery] = useState("");
 	const [query, setQuery] = useState<string | undefined>();
-	const [showSubmitAnimation, setShowSubmitAnimation] = useState(false);
 	const [selectedPosts, setSelectedPosts] = useState<string[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<string | undefined>();
 
 	const [currentSite, setCurrentSite] = useState<string>("safebooru.org");
 
-	const fetchPosts = async () => {
-		setShowSubmitAnimation(true);
-		setTimeout(() => setShowSubmitAnimation(false), 1500);
+	const fetchPosts = async ({ attempts = 0, maxAttempts = 3 } = {}) => {
+		setLoading(true);
 		const tags = query?.split(" ") ?? [];
 		const results = await booruSearch(currentSite, tags, {
 			page: page,
 			limit: 25,
 		});
-		if (results) {
+		if (results.length > 0) {
 			setPosts(results);
 			setSelectedPosts([]);
+			setLoading(false);
+			setError(undefined);
+		} else {
+			if (attempts < maxAttempts) {
+				setError(
+					`Attempt ${attempts + 1} of ${maxAttempts} failed. Retrying...`,
+				);
+				setTimeout(() => {
+					fetchPosts({ attempts: attempts + 1 });
+				}, 1000);
+			} else {
+				setPosts([]);
+				setSelectedPosts([]);
+				setLoading(false);
+				setError("No results found.");
+			}
 		}
 	};
 
@@ -45,17 +77,41 @@ export const App = () => {
 		}
 	}, [page, query, currentSite]);
 
-	console.info(posts);
-
-	useEffect(() => {
-		if (posts.length > 0) {
-			topRef.current.scrollIntoView({ behavior: "smooth" });
-		}
-	}, [posts]);
-
-	useEffect(() => {
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setQuery(tempQuery);
 		setPage(0);
-	}, [currentSite]);
+	};
+
+	const handleChangeSite = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setCurrentSite(e.target.value);
+		setPage(0);
+	};
+
+	const handleIncrementPage = () => {
+		setPage((page) => page + 1);
+		topRef.current.scrollIntoView();
+	};
+
+	const handleDecrementPage = () => {
+		setPage((page) => page - 1);
+		topRef.current.scrollIntoView();
+	};
+
+	const handleSelectPost = (post_id: string) => {
+		if (!selectedPosts.includes(post_id)) {
+			setTimeout(() => {
+				document
+					.getElementById(post_id)
+					?.scrollIntoView({ behavior: "smooth", block: "center" });
+			}, 0);
+		}
+		setSelectedPosts((prev) =>
+			prev.includes(post_id)
+				? prev.filter((id) => id !== post_id)
+				: [...prev, post_id],
+		);
+	};
 
 	return (
 		<div ref={topRef} className="select-none">
@@ -64,11 +120,7 @@ export const App = () => {
 				className="bg-white border-b border-gray-200 p-2 w-full sticky top-0 z-10"
 			>
 				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						setQuery(tempQuery);
-						setPage(0);
-					}}
+					onSubmit={handleSubmit}
 					className="flex items-center gap-2 w-full flex-col md:flex-row"
 				>
 					<div className="flex gap-2 items-center w-full">
@@ -77,11 +129,12 @@ export const App = () => {
 							placeholder="Search..."
 							type="search"
 							onChange={(e) => setTempQuery(e.currentTarget.value)}
+							value={tempQuery}
 						/>
 						<select
 							className="border border-gray-300 rounded p-2 focus:border-blue-500 focus:outline-none"
 							value={currentSite}
-							onChange={(e) => setCurrentSite(e.target.value)}
+							onChange={handleChangeSite}
 						>
 							{Object.keys(sites).map((key) => {
 								return (
@@ -96,7 +149,7 @@ export const App = () => {
 							className={classNames(
 								"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
 								{
-									"animate-pulse": showSubmitAnimation,
+									"animate-pulse": loading,
 								},
 							)}
 						>
@@ -107,9 +160,7 @@ export const App = () => {
 						<button
 							type="button"
 							className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
-							onClick={() => {
-								setPage(page - 1);
-							}}
+							onClick={handleDecrementPage}
 							disabled={page === 0}
 						>
 							⮜
@@ -118,9 +169,7 @@ export const App = () => {
 						<button
 							type="button"
 							className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-							onClick={() => {
-								setPage(page + 1);
-							}}
+							onClick={handleIncrementPage}
 						>
 							⮞
 						</button>
@@ -128,43 +177,66 @@ export const App = () => {
 				</form>
 			</header>
 
-			<main className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-				{posts.map((post) => (
-					<img
-						id={post.id}
-						key={post.id}
-						className={classNames("cursor-pointer", {
-							"max-w-full max-h-screen col-span-2 md:col-span-3 lg:col-span-4":
-								selectedPosts.includes(post.id),
-							"object-cover w-full aspect-square": !selectedPosts.includes(
-								post.id,
-							),
-						})}
-						src={
-							selectedPosts.includes(post.id)
-								? post.fileUrl
-								: post?.previewUrl ?? post?.sampleUrl ?? post.fileUrl
-						}
-						loading="lazy"
-						alt={`${post.id}`}
-						title={`url: ${post.fileUrl}\n\ntags: ${post.tags.join(" ")}`}
-						onClick={() => {
-							if (!selectedPosts.includes(post.id)) {
-								setTimeout(() => {
-									document
-										.getElementById(post.id)
-										?.scrollIntoView({ behavior: "smooth", block: "center" });
-								}, 0);
+			{loading && (
+				<LoadingIndicator className="text-center m-6 text-blue-500" />
+			)}
+
+			{!loading && (
+				<main className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+					{posts.map((post) => (
+						<img
+							id={post.id}
+							key={post.id}
+							className={classNames("cursor-pointer", {
+								"max-w-full max-h-screen col-span-2 md:col-span-3 lg:col-span-4":
+									selectedPosts.includes(post.id),
+								"object-cover w-full aspect-square": !selectedPosts.includes(
+									post.id,
+								),
+							})}
+							src={
+								selectedPosts.includes(post.id)
+									? post.fileUrl
+									: post?.previewUrl ?? post?.sampleUrl ?? post.fileUrl
 							}
-							setSelectedPosts((prev) =>
-								prev.includes(post.id)
-									? prev.filter((id) => id !== post.id)
-									: [...prev, post.id],
-							);
-						}}
-					/>
-				))}
-			</main>
+							loading="lazy"
+							alt={`${post.id}`}
+							title={`url: ${post.fileUrl}\n\ntags: ${post.tags.join(" ")}`}
+							onClick={() => handleSelectPost(post.id)}
+						/>
+					))}
+				</main>
+			)}
+
+			{error && <div className="text-center m-6">{error}</div>}
+
+			{query === undefined && (
+				<div className="flex items-center justify-center m-6">
+					<div className="text-left">
+						<h1 className="text-4xl font-semibold mb-2">Quick Start</h1>
+						<ul className="list-disc">
+							<li>
+								Type a tag into the search bar at the top, such as{" "}
+								<button
+									type="button"
+									className="text-blue-500"
+									onClick={() => setTempQuery("landscape")}
+								>
+									landscape
+								</button>
+								.
+							</li>
+							<li>Press the blue search button.</li>
+							<li>Click on an image to make it full-sized.</li>
+							<li>Click on it again to make it small.</li>
+							<li>
+								The booru can be changed by selecting a different one. It will
+								be automatically switched.
+							</li>
+						</ul>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
