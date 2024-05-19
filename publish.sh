d0757410617704dffff4874852c03a7ede57bf0f
pikdum <pikdum@kuudere.moe>
2024-05-19 15:57:31 -0500

    fix: windows build

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -eux
 
+# create a new WINEPREFIX to avoid error:
+# errorOut=wine: '/github/home' is not owned by you, refusing to create a configuration directory there
+export WINEPREFIX="$(mktemp -d)"
+trap 'rm -rf "$WINEPREFIX"' EXIT
+
 npm install
 npm run build
 rm -rf dist/win-unpacked
