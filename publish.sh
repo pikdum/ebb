7db4e2bc71a68517d906ba60858aa33827983fa3
pikdum <pikdum@kuudere.moe>
2024-05-26 13:47:12 -0500

    fix: windows auto-updating

--- publish.sh
+++ publish.sh
@@ -11,4 +11,6 @@ export NODE_ENV=production
 npm run build
 rm -rf dist/win-unpacked
 rm -rf dist/linux-unpacked
+# fix filenames so windows auto-updater doesn't 404
+find . -type f -name "* *" -exec bash -c 'mv "$0" "${0// /-}"' {} \;
 gh release upload "v$(node -p "require('./package.json').version")" dist/* --clobber
