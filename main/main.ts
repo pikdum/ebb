fe2cad63d4bc5d1701598131abd2c18f20bc197e
pikdum <pikdum@kuudere.moe>
2025-06-13 16:25:53 -0500

    fix: catch errors from auto-update

--- main/main.ts
+++ main/main.ts
@@ -93,9 +93,13 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", async () => {
-	await autoUpdater.checkForUpdatesAndNotify();
+app.on("ready", () => {
 	createWindow();
+	try {
+		autoUpdater.checkForUpdatesAndNotify();
+	} catch (error) {
+		console.error("Error checking for updates:", error);
+	}
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
